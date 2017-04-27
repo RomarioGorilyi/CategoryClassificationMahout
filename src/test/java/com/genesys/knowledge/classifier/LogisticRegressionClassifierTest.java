b3abaf065639526c5a34b5c51bfdbe867470434f
Roman Horilyi <gorilyiroman@gmail.com>
2017-04-27 21:06:30 +0300

    add classification test

--- src/test/java/com/genesys/knowledge/classifier/LogisticRegressionClassifierTest.java
+++ src/test/java/com/genesys/knowledge/classifier/LogisticRegressionClassifierTest.java
@@ -1,5 +1,8 @@
 package com.genesys.knowledge.classifier;
 
+import com.genesys.knowledge.classifier.exception.CategoryNotFoundException;
+import com.genesys.knowledge.domain.Category;
+import com.genesys.knowledge.domain.Document;
 import org.junit.Test;
 
 /**
@@ -9,6 +12,22 @@ public class LogisticRegressionClassifierTest {
 
     @Test
     public void testClassification() {
+        LogisticRegressionClassifier classifier = new LogisticRegressionClassifier();
 
+        String documentBody = "Test text";
+        Category category = new Category("test_id");
+        Category[] categories = {category};
+        Document document = new Document(documentBody, categories);
+
+        for (int trainLoop = 0; trainLoop < 30; trainLoop++) {
+            classifier.train(document);
+        }
+
+        try {
+            double categoryProbability = classifier.calculateCategoryProbability(document, category);
+            System.out.println("Category probability: " + categoryProbability);
+        } catch (CategoryNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
