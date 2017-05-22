05aba87b16f5ac8bae6844a11cd4e033c7b1d55e
Roman Horilyi <gorilyiroman@gmail.com>
2017-05-22 16:29:11 +0300

    add confidence-precision test for classifiers

--- src/test/java/com/genesys/knowledge/classification/classifier/LrClassifierConstructedWithDocumentsTest.java
+++ src/test/java/com/genesys/knowledge/classification/classifier/LrClassifierConstructedWithDocumentsTest.java
@@ -3,10 +3,15 @@ package com.genesys.knowledge.classification.classifier;
 import com.genesys.knowledge.classification.util.DocumentHandler;
 import com.genesys.knowledge.domain.Category;
 import com.genesys.knowledge.domain.Document;
+import org.apache.mahout.math.Vector;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -19,35 +24,11 @@ public class LrClassifierConstructedWithDocumentsTest {
 
     @Before
     public void retrieveDocuments() {
-//        String urlTemplate = "http://gks-dep-stbl:9092/gks-server/v2/knowledge/tenants/1/langs/en_US/documents?size=%d&from=%d";
-//        documents = new ArrayList<>(1000);
-//
-//        for (int i = 0; i < 3; i++) {
-//            String url = String.format(urlTemplate, 200, i * 200);
-//            documents.addAll(DocumentHandler.retrieveDocuments(url));
-//        }
-//
-//        prepareDocuments();
-
         String url = "http://gks-dep-stbl:9092/gks-server/v1/kbs/langs/en/documents?tenantId=1&size=2000";
         String knowledgeBase = "bank_of_america";
         documents = DocumentHandler.retrieveDocuments(url, knowledgeBase);
 
         prepareDocuments();
-    }
-
-
-    @Test
-    public void testClassifier() { // TODO split method into separate test methods in order to benchmark performance with JMH
-        Collections.shuffle(documents, new SecureRandom());
-        List<Document> trainingDocuments = documents.subList(0, 4 * documents.size() / 5);
-        List<Document> testDocuments = documents.subList(4 * documents.size() / 5, documents.size());
-
-        int[] trainingLoopsNumbers = {10, 30, 50, 100};
-        for (int i = 0; i < trainingLoopsNumbers.length; i++) {
-            System.out.println("Number of training laps: " + trainingLoopsNumbers[i]);
-            testClassifier(trainingDocuments, testDocuments, trainingLoopsNumbers[i]);
-        }
     }
 
     private void prepareDocuments() {
@@ -60,6 +41,68 @@ public class LrClassifierConstructedWithDocumentsTest {
         System.out.println(documents.size());
     }
 
+    @Test
+    public void testClassifierWithDifferentNumberOfTrainingLaps() {
+        Collections.shuffle(documents, new SecureRandom());
+        List<Document> trainingDocuments = documents.subList(0, 4 * documents.size() / 5);
+        List<Document> testDocuments = documents.subList(4 * documents.size() / 5, documents.size());
+
+        int[] trainingLapsNumbers = {10, 30, 50};
+        for (int trainingLapsNumber : trainingLapsNumbers) {
+            System.out.println("Number of training laps: " + trainingLapsNumber);
+            testClassifier(trainingDocuments, testDocuments, trainingLapsNumber);
+        }
+    }
+
+    @Test
+    public void testClassifierCreatingConfidencePrecisionStatistics() {
+        Collections.shuffle(documents, new SecureRandom());
+        List<Document> trainingDocuments = documents.subList(0, 4 * documents.size() / 5);
+        List<Document> testDocuments = documents.subList(4 * documents.size() / 5, documents.size());
+
+        List<List<Integer>> statistics = new ArrayList<>(101);
+        for (int i = 0; i <= 100; i++) {
+            statistics.add(Arrays.asList(0, 0));
+        }
+
+        for (int run = 0; run < 50; run++) {
+            LogisticRegressionClassifier classifier = new LogisticRegressionClassifier(trainingDocuments);
+            classifier.getCategoryHandler().clear();
+
+            // train
+            trainClassifier(classifier, trainingDocuments, 30);
+
+            // evaluate
+            for (Document document : testDocuments) {
+                Vector classificationVector = classifier.classifyDocument(document);
+                for (Vector.Element element : classificationVector.all()) {
+                    String classifiedCategoryId = classifier.getCategoryHandler().getCategoryId(element.index());
+                    int confidence = (int) Math.round(element.get() * 100); // convert into percentage
+
+                    statistics.get(confidence).set(1, statistics.get(confidence).get(1) + 1); // increment number of all predictions
+                    if (document.getCategories().contains(new Category(classifiedCategoryId))) {
+                        statistics.get(confidence).set(0, statistics.get(confidence).get(0) + 1); // increment number of successful predictions
+                    }
+                }
+            }
+        }
+
+        String csvFile = "src/main/resources/confidence-precision-statistics.csv";
+        try (PrintWriter writer = new PrintWriter(csvFile)) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < statistics.size(); i++) {
+                List<Integer> precisionPair = statistics.get(i);
+                double precision = ((double) precisionPair.get(0)) / precisionPair.get(1) * 100;
+                sb.append(i).append(',')
+                        .append(precision).append('\n');
+            }
+            writer.write(sb.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // TODO rename method 'cause it's confusing (note phases of classification process: training -> testing)
     private void testClassifier(List<Document> trainingDocuments,
                                 List<Document> testDocuments,
                                 int trainingLapsNumber) {
@@ -80,7 +123,7 @@ public class LrClassifierConstructedWithDocumentsTest {
         evaluateClassification(testDocuments, numberOfCorrectClassifications);
     }
 
-    private void trainClassifier(LogisticRegressionClassifier classifier,
+    private void trainClassifier(AbstractClassifier classifier,
                                  List<Document> trainingDocuments,
                                  int trainingLapsNumber) {
         for (int i = 0; i < trainingLapsNumber; i++) {
@@ -121,4 +164,5 @@ public class LrClassifierConstructedWithDocumentsTest {
                     + " times");
         }
     }
+
 }
