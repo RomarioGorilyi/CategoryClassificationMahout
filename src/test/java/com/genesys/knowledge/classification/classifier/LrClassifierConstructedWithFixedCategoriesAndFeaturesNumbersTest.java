59789340a7bb51782ac0c2f0a9ed39aae23579ee
Roman Horilyi <gorilyiroman@gmail.com>
2017-05-31 17:05:24 +0300

    refactor: get rid of documents

--- src/test/java/com/genesys/knowledge/classification/classifier/LrClassifierConstructedWithFixedCategoriesAndFeaturesNumbersTest.java
+++ src/test/java/com/genesys/knowledge/classification/classifier/LrClassifierConstructedWithFixedCategoriesAndFeaturesNumbersTest.java
@@ -1,15 +1,13 @@
 package com.genesys.knowledge.classification.classifier;
 
-import com.genesys.knowledge.classification.util.DocumentHandler;
 import com.genesys.knowledge.domain.Category;
-import com.genesys.knowledge.domain.Document;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -28,62 +26,56 @@ public class LrClassifierConstructedWithFixedCategoriesAndFeaturesNumbersTest {
 
     @Test
     public void testTrainClassifierWithTwoDocs() {
-        Map<Document, List<String>> documentTokens = new HashMap<>();
+        List<List<String>> tokensList = new ArrayList<>();
 
         Category category1 = new Category("category1");
-        Document document1 = new Document("Test text to teach machine.");
-        List<String> tokens1 = DocumentHandler.convertTextToTokens(document1.getText(), null);
-        documentTokens.put(document1, tokens1);
-        trainClassifier(tokens1, category1, documentTokens.values());
+        List<String> tokens1 = Arrays.asList("test", "text", "teach", "machine");
+        tokensList.add(tokens1);
+        trainClassifier(tokens1, category1, tokensList);
 
         Category category2 = new Category("category2");
-        Document document2 = new Document("Real document that can be really applied.");
-        List<String> tokens2 = DocumentHandler.convertTextToTokens(document2.getText(), null);
-        documentTokens.put(document2, tokens2);
-        trainClassifier(tokens2, category2, documentTokens.values());
+        List<String> tokens2 = Arrays.asList("real", "document", "apply");
+        tokensList.add(tokens2);
+        trainClassifier(tokens2, category2, tokensList);
 
         String expectedAlreadyTrainedDocumentCategory = category1.getId();
-        String actualAlreadyTrainedDocumentCategory = classifier.calcMostConfidentCategory(tokens1, documentTokens.values());
+        String actualAlreadyTrainedDocumentCategory = classifier.calcMostConfidentCategory(tokens1, tokensList);
         assertEquals(expectedAlreadyTrainedDocumentCategory, actualAlreadyTrainedDocumentCategory);
 
-        Document testDocument = new Document("Teaching machines is our hobby");
-        List<String> testTokens = DocumentHandler.convertTextToTokens(testDocument.getText(), null);
-        documentTokens.put(testDocument, testTokens);
+        List<String> testTokens = Arrays.asList("teach", "machine", "hobby");
+        tokensList.add(testTokens);
         String expectedTestDocumentCategory = category1.getId();
-        String actualTestDocumentCategory = classifier.calcMostConfidentCategory(testTokens, documentTokens.values());
+        String actualTestDocumentCategory = classifier.calcMostConfidentCategory(testTokens, tokensList);
         assertEquals(expectedTestDocumentCategory, actualTestDocumentCategory);
     }
 
     @Test
     public void testTrainClassifierWithOnlyOneDoc() {
-        Map<Document, List<String>> documentTokens = new HashMap<>();
+        List<List<String>> tokensList = new ArrayList<>();
 
         Category category = new Category("category");
-        Document document = new Document("Test text");
-        List<String> tokens = DocumentHandler.convertTextToTokens(document.getText(), null);
-        documentTokens.put(document, tokens);
-        trainClassifier(tokens, category, documentTokens.values());
+        List<String> tokens = Arrays.asList("test", "text");
+        tokensList.add(tokens);
+        trainClassifier(tokens, category, tokensList);
 
-        Document testDocument = new Document("Teaching machines is our hobby");
-        List<String> testTokens = DocumentHandler.convertTextToTokens(testDocument.getText(), null);
-        documentTokens.put(testDocument, testTokens);
+        List<String> testTokens = Arrays.asList("teach", "machine", "hobby");
+        tokensList.add(testTokens);
         String expectedTestDocumentCategory = category.getId();
-        String actualTestDocumentCategory = classifier.calcMostConfidentCategory(testTokens, documentTokens.values());
+        String actualTestDocumentCategory = classifier.calcMostConfidentCategory(testTokens, tokensList);
         assertEquals(expectedTestDocumentCategory, actualTestDocumentCategory);
     }
 
     @Test
     public void testCalculateUnknownCategoryProbability() {
-        Map<Document, List<String>> documentTokens = new HashMap<>();
+        List<List<String>> tokensList = new ArrayList<>();
 
         Category category = new Category("category");
-        Document document = new Document("Test text");
-        List<String> tokens = DocumentHandler.convertTextToTokens(document.getText(), null);
-        documentTokens.put(document, tokens);
-        trainClassifier(tokens, category, documentTokens.values());
+        List<String> tokens = Arrays.asList("test", "text");
+        tokensList.add(tokens);
+        trainClassifier(tokens, category, tokensList);
 
         Category unknownCategory = new Category("unknownCategory");
-        double actualProbability = classifier.calcCategoryProbability(tokens, unknownCategory, documentTokens.values());
+        double actualProbability = classifier.calcCategoryProbability(tokens, unknownCategory, tokensList);
         double expectedProbability = 0;
         assertEquals(expectedProbability, actualProbability, 0);
     }
